57722e534025431082fe7e0d7971ead9e59b32a6
Amos <amosakinwale4@gmail.com>
2022-11-03 16:45:47 +0100

    designed my dashboard and deleting cart items

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,188 +1,185 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
-import commercesignup from "../images/commercesignup.jpg"
-import  { useState } from 'react';
+import commercesignup from "../images/commercesignup.jpg";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import {
-    FaTh,
-    FaBars,
-    FaUserAlt,
-    FaRegChartBar,
-    FaCommentAlt,
-    FaShoppingBag,
-    FaThList
-}from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
+  FaTh,
+  FaBars,
+  FaUserAlt,
+  FaRegChartBar,
+  FaCommentAlt,
+  FaShoppingBag,
+  FaThList,
+} from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 import { useEffect } from "react";
 import axios from "axios";
 
 const Dashboard = () => {
-  const [selectedproductuid, setselectedproductuid] = useState("")
+  const [selectedproductuid, setselectedproductuid] = useState("");
   useEffect(() => {
-    getallProducts()
-    
-    }, [])
-  
-  
-  const [productsarray, setproductsarray] = useState([])
+    getallProducts();
+  }, []);
 
-  const getallProducts = ()=>{
-    const productsPoint = "http://localhost:5008/product/displayallproducts" 
-    axios.get(productsPoint).then((result)=>{
-      console.log(result)
-      setproductsarray(result.data.result)
+  const [productsarray, setproductsarray] = useState([]);
 
-    })
-
-
-  }
-  const addtocart= (uid)=>{
+  const getallProducts = () => {
+    const productsPoint = "http://localhost:5008/product/displayallproducts";
+    axios.get(productsPoint).then((result) => {
+      console.log(result);
+      setproductsarray(result.data.result);
+    });
+  };
+  const addtocart = (uid) => {
     // setselectedproductuid(e)
 
-   let token = localStorage.token
-    const cartPoint = "http://localhost:5008/cart/savecart" 
-    let cartdetails = {uid, token}
-    axios.post(cartPoint, cartdetails).then((result)=>{
-      console.log(result)
-    })
+    let token = localStorage.token;
+    const cartPoint = "http://localhost:5008/cart/savecart";
+    let cartdetails = { uid, token };
+    axios.post(cartPoint, cartdetails).then((result) => {
+      console.log(result);
+    });
     // console.log("good")
-    console.log(uid)
+    console.log(uid);
+  };
 
-    
- 
-  }
-
-  const[isOpen ,setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
 
   // const toggle = () => setIsOpen (!isOpen);
-  const menuItem=[
-      {
-          path:"/dashboard",
-          name:"Dashboard",
-          icon:<FaTh/>
-      },
-      {
-          path:"/about",
-          name:"About",
-          icon:<FaUserAlt/>
-      },
-      {
-          path:"/analytics",
-          name:"Analytics",
-          icon:<FaRegChartBar/>
-      },
-      {
-          path:"/comment",
-          name:"Comment",
-          icon:<FaCommentAlt/>
-      },
-      {
-          path:"/product",
-          name:"Product",
-          icon:<FaShoppingBag/>
-      },
-      {
-          path:"/productList",
-          name:"Product List",
-          icon:<FaThList/>
-      }
-  ]
-  
+  const menuItem = [
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      icon: <FaTh />,
+    },
+    {
+      path: "/about",
+      name: "About",
+      icon: <FaUserAlt />,
+    },
+    {
+      path: "/analytics",
+      name: "Analytics",
+      icon: <FaRegChartBar />,
+    },
+    {
+      path: "/comment",
+      name: "Comment",
+      icon: <FaCommentAlt />,
+    },
+    {
+      path: "/product",
+      name: "Product",
+      icon: <FaShoppingBag />,
+    },
+    {
+      path: "/productList",
+      name: "Product List",
+      icon: <FaThList />,
+    },
+  ];
+
   return (
     <>
-        <div className="topdashboard">
-            <p>YOUR EVERYDAY, DELIVERED <span>FOR FREE</span></p>
+      <div className="topdashboard">
+        <p>
+          YOUR EVERYDAY, DELIVERED <span>FOR FREE</span>
+        </p>
+      </div>
+      <div className="bodydashboard d-flex">
+        <div className="d-none d-md-block">
+          <Sidebar />
+        </div>
 
-          </div>
-    <div className="container-fluid ">
-      <div className="row">
-        <div className="col-md-2  dashboardbody  d-none d-md-block">
-        <div className="container-sidenav ">
-           <div style={{width: isOpen ? "100%" : "50px"}} className="sidebar">
-               <div className="top_section">
+        <div className="container-fluid ">
+          <div className="row">
+            {/* <div className="col-md-2   d-none d-md-block"> */}
+
+            {/* <div className="container-sidenav ">
+           <div style={{width: isOpen ? "100%" : "50px"}} className="sidebar"> */}
+            {/* <div className="top_section">
                    <p style={{display: isOpen ? "block" : "none"}} className="logo">amoscart</p>
                   
-               </div>
-               {
+               </div> */}
+            {/* {
                    menuItem.map((item, index)=>(
                        <NavLink to={item.path} key={index} className="link" activeclassName="active">
                            <div className="icon">{item.icon}</div>
                            <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
                        </NavLink>
                    ))
-               }
-           </div>
-           {/* <main>{children}</main> */}
-        </div>
-       
-        </div>
-    
-        <div className="col-md-10 bg-light">
-          <div className="container-fluid my-2 bg-white">
-            <div className="row">
-              <div className="col-md-5 col-9 d-flex border border-dark border-1 mx-1 align-items-center">
-              <input type="text" placeholder="type your search here..." className="border-0 w-100 searchinput"/>
-              <i class="fa fa-search" aria-hidden="true"></i>
+               } */}
+            {/* </div> */}
+            {/* <main>{children}</main> */}
+            {/* </div> */}
+            {/* </div> */}
+
+            <div className="col-md-12  bg-light">
+              <div className="container-fluid my-2 bg-white">
+                <div className="row">
+                  <div className="col-md-5 col-8 d-flex border border-dark border-1 mx-1 align-items-center">
+                    <input
+                      type="text"
+                      placeholder="type your search here..."
+                      className="border-0 w-100 searchinput"
+                    />
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                  </div>
+                  <div className="col-md-1 btn btn-warning col-3">search</div>
+                  <div className="col-md-5 d-flex justify-content-end">
+                    <b className="mx-1">Account</b>
+                    <i class="fa-solid fa-user" id="topicons"></i>
+                    <b className="mx-1">shop</b>
+                    <i class="fa-brands fa-shopify" id="topicons"></i>
+                    <Link
+                      to={"/usercart"}
+                      style={{ textDecoration: "none", color: "black" }}
+                    >
+                      <b>Cart</b>
+                      <i class="fa-solid fa-cart-shopping" id="topicons"></i>
+                    </Link>
+                  </div>
+                </div>
               </div>
-              <div className="col-md-1 btn btn-warning col-2">search</div>
-              <div className="col-md-5 d-flex justify-content-end">
-                <b className="mx-1">Account</b><i class="fa-solid fa-user" id="topicons"></i>
-                <b className="mx-1">shop</b><i class="fa-brands fa-shopify" id="topicons"></i>
-                <Link to={"/usercart"} style={{textDecoration:"none", color:"black"}}><b>Cart</b><i class="fa-solid fa-cart-shopping" id="topicons"></i></Link>
+
+              <div className="container-fluid mt-3">
+                <div className="row  justify-content-around bg-white">
+                  {productsarray.map((product, index) => (
+                    <>
+                      <div className="col-md-2 col-5  card productdiv shadow pb-3  my-2">
+                        <div className="align-self-center bg-info h-50">
+                          <img
+                            src={product.imageurl}
+                            className="w-100 h-100 "
+                            alt=""
+                          />
+                        </div>
+
+                        <div className="productname text-center">
+                          <p className="producttext">${product.price}.</p>
+                          <b className=" ">{product.productname}</b>
+                          <p className="  productdescription">
+                            {product.description}
+                          </p>
+                        </div>
+                        <br />
+
+                        <button
+                          className="btn  btn-dark shadow text-bold h-100 w-100 align-self-center"
+                          onClick={() => addtocart(product.uid)}
+                        >
+                          add to cart
+                        </button>
+                      </div>
+                    </>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-
-       <div className="container-fluid mt-3">
-        <div className="row  justify-content-between bg-white">
-        {
-          productsarray.map((product, index)=>(
-            <>
-            <div className="col-md-3 mx-1 col-5  card productdiv shadow pb-5  my-2">
-              <img src={product.imageurl} className='w-75 align-self-center h-75' alt="" />
-          
-             <div className="d-flex align-self-center productname">
-             <p className="text-center">${product.price}.</p>
-             <b className="mx-1 text-center ">{product.productname}</b>
-           
-              
-           
-               </div>
-               <p className="text-center  productdescription">{product.description}</p><br />
-              
-              {/* <button className='btn btn-dark shadow text-bold w-75 align-self-center' onClick={(e)=>addtocart(setselectedproductuid(e.target.value))} value={product.uid}>add to cart</button> */}
-              <button className='btn btn-dark shadow text-bold w-75 align-self-center'  onClick={()=>addtocart(product.uid)} >add to cart</button>
-          
-            </div>
-            
-
-
-            </>
-          ))
-         }
-          
-        </div>
-       </div>
-      
-        
-
-       
-          {/* <div className="container mt-5">
-            <div className="row">
-              <div className="col-md-3 card">
-                <img src={commercesignup} className='w-100 h-100' alt="" />
-                <p>$2550</p>
-                <b> amoled wrist watch</b>
-                <button className='btn btn-dark shadow w-75 align-self-center'><b>add to cart</b></button>
-              </div>
-            </div>
-          </div> */}
-        
         </div>
       </div>
-  
-    </div>
- 
     </>
   );
 };
--- src/pages/Usercart.js
+++ src/pages/Usercart.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import { useRef } from 'react'
 import axios from 'axios'
 
 const Usercart = () => {
   const [cartarray, setcartarray] = useState([])
+  const [message, setmessage] = useState("")
+const [checkoutprice, setcheckoutprice] = useState(0)
+  const [price, setprice] = useState(0)
+  let addpriceArray = []
 
   useEffect(() => {
    getcartdetails()
@@ -16,36 +21,73 @@ const Usercart = () => {
     console.log(token)
     let cartPoint = "http://localhost:5008/cart/cartdetails" 
     axios.post(cartPoint, finddetails).then((result)=>{
-      console.log(result)
+      // console.log(result)
       setcartarray(result.data.result)
+      addpriceArray = result.data.result
+      console.log(addpriceArray)
+      let totalprice = addpriceArray.reduce((total, item)=>{
+        return total + Number(item.itemprice)
+      }, 0);
+      setcheckoutprice(totalprice)
+      console.log(totalprice)
     })
-    
-
-
   }
+  const processPayment=()=>{
+    alert("are you sure you want to pay" + " " + "$" + checkoutprice)
+  }
+
+  const deleteItem = (id)=>{
+    let deleteDeets = {id}
+    let deletePoint = "http://localhost:5008/cart/deleteitem"
+    axios.post(deletePoint, deleteDeets).then((result)=>{
+      console.log("good", result)
+      setmessage(result.data.message)
+      
+    })
+    getcartdetails()
+    console.log(id)
+  }
+
   return (
     <>
-    <div className='bg-light cartbody'>
+    
+    <div className={message?'alert alert-success':'d-none'}>{message}</div>
+    <div className='bg-light cartbody cartheight'>
       <div className='col-md-7 mx-auto'>
         <div className='container-fluid'>
         {
             cartarray.map((product, number)=>(
             <>
-            <div className='row'>
-              <div className='col-md-6 bg-light'>
-                <img src={product.cartimage} alt="" className='h-100 shadow mt-3 bg-dark w-75' />
+            <div className='row bg-white shadow pt-3 mt-1 pb-4'>
+              <div className='col-md-6 bg-white'>
+                <img src={product.cartimage} alt="" className='h-100 bg-dark w-75' />
+              </div>
+              <div className='col-md-4'>
+                <p>{product.itemdescription}</p>
+                <b>{product.itemname}</b><br />
+                <b>${product.itemprice}</b><br />
+               {/* <span className='btn cartbutton '>checkout $<input type="button" className='btn' value={product.itemprice} /></span>  */}
+                <button className='btn  cartbutton mt-3 shadow'  value={product.itemprice}>checkout ${product.itemprice}</button>
+                <button className='btn rounded-5 h-25 bg-info mt-2 ' onClick={()=>deleteItem(product._id)}>delete product</button>
               </div>
             </div>
+            
             </>
 
             ))
 
         }
+        
 
         </div>
+        <div className='row justify-content-center'>
+        <input  type="button" className='btn w-75  bg-danger shadow' onClick={processPayment} value={checkoutprice}/>
+        </div>
+      
       </div>
-   
-
+      {/* <div className='row justify-content-center'> */}
+      
+      {/* </div> */}
     </div>
     </>
   )
